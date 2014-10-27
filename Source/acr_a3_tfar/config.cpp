d16548b0bc471d2c4dd4ff5bd0179df4301a8240
EvroMalarkey <evromalarkey@gmail.com>
2014-10-27 15:45:28 +0100

    Added: tfar radios

--- Source/acr_a3_tfar/config.cpp
+++ Source/acr_a3_tfar/config.cpp
@@ -1,42 +1,40 @@
 class CfgPatches
 {
-	class task_force_radio_items
+	class acr_a3_tfar
 	{
 		units[] = {
-			"tf_rt1523g_big_acr_a3",
-			"tf_rt1523g_acr_a3"
+			"tf_anprc155_big_acr_a3",
+			"tf_anprc155_acr_a3"
 		};
 	};
 };
 class CfgVehicles {
-    class tf_rt1523g;
-	class tf_rt1523g_acr_a3: tf_rt1523g
+    class tf_anprc155;
+	class tf_anprc155_big_acr_a3: tf_anprc155
 	{
-		displayName = "RT-1523G (ASIP) ACR";
-		descriptionShort = "RT-1523G (ASIP) ACR long range radio 20km";
+		displayName = "AN/PRC 155 Big ACR";
+		descriptionShort = "AN/PRC 155 Big ACR long range radio 20km";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\acr_a3_tfar\data\chcipnievro.paa"};
+        hiddenSelectionsTextures[] = {"\acr_a3_tfar\data\clf_nicecomm2_big_ACR_vz95_co.paa"};
 	};
-	class tf_rt1523g_big_acr_a3: tf_rt1523g {
-		displayName = "RT-1523G (ASIP) Big ACR";
-		descriptionShort = "RT-1523G (ASIP) Big ACR long range radio 20km";
-		maximumLoad = 160;
-		mass = 30;		
+	class tf_anprc155_acr_a3: tf_anprc155 {
+		displayName = "AN/PRC 155 ACR";
+		descriptionShort = "AN/PRC 155 ACR long range radio 20km";
 		hiddenSelections[] = {"camo"};
-        hiddenSelectionsTextures[] = {"\acr_a3_tfar\data\clf_nicecomm2_ACR_vz95_co.paa"};
-		model="\task_force_radio_items\models\clf_nicecomm2";
+		hiddenSelectionsTextures[] = {"\acr_a3_tfar\data\clf_nicecomm2_ACR_vz95_co.paa"};
+        model="\task_force_radio_items\models\clf_prc117g_ap";
 	};
 
-	class Box_NATO_Support_F;
-    class TF_NATO_Radio_Crate: Box_NATO_Support_F
+	class Box_IND_Support_F;
+    class TF_IND_Radio_Crate: Box_IND_Support_F
     {
     	class TransportBackpacks
     	{
-    		class _xx_tf_rt1523g_big_acr_a3 {
+    		class _xx_tf_anprc155_big_acr_a3 {
     			count = 1;
     			backpack = "tf_rt1523g_big_acr_a3";
     		};
-    		class _xx_tf_rt1523g_acr_a3 {
+    		class _xx_tf_anprc155_acr_a3 {
     			count = 1;
     			backpack = "tf_rt1523g_acr_a3";
     		};
