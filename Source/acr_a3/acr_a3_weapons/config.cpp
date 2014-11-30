3152b46d7408772c965afc83506a2bf601d79268
EvroMalarkey <evromalarkey@gmail.com>
2014-11-30 13:07:58 +0100

    Version 0.6 RC2

--- Source/acr_a3/acr_a3_weapons/config.cpp
+++ Source/acr_a3/acr_a3_weapons/config.cpp
@@ -6,8 +6,219 @@ class CfgPatches
 		units[] = { };
 		weapons[] = { };
 		requiredVersion = 0.100000;
-		requiredAddons[] = { };
+		requiredAddons[] = {"CUP_Weapons_WeaponsCore"};
 	};
+};
+
+class CfgWeapons
+{
+    class CUP_srifle_CZ750;
+	class acr_a3_CZ750: CUP_srifle_CZ750
+	{
+		scope = 1;
+	};
+	class CUP_srifle_CZ750_SOS_bipod;
+    class acr_a3_CZ750_SOS: CUP_srifle_CZ750_SOS_bipod {};
+    class CUP_arifle_CZ805_A2;
+    class acr_a3_CZ805_A2: CUP_arifle_CZ805_A2
+    {
+        scope = 1;
+    };
+    class CUP_arifle_CZ805_A1;
+    class acr_a3_CZ805_A1: CUP_arifle_CZ805_A1
+    {
+        scope = 1;
+    };
+    class CUP_arifle_CZ805_GL;
+    class acr_a3_CZ805_GL: CUP_arifle_CZ805_GL
+    {
+        scope = 1;
+    };
+    class acr_a3_CZ805_A2_Holo_Laser: acr_a3_CZ805_A2
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_Holosight";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_A1_Holo_Laser: acr_a3_CZ805_A1
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_Holosight";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_A2_Aco_Laser: acr_a3_CZ805_A2
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_Aco";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_A1_Aco_Laser: acr_a3_CZ805_A1
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_Aco";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_A1_MRCO_Laser: acr_a3_CZ805_A1
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_MRCO";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_A2_MRCO_Laser: acr_a3_CZ805_A1
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_MRCO";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class acr_a3_CZ805_GL_Hamr_Laser: acr_a3_CZ805_GL
+    {
+        class LinkedItems
+        {
+            class LinkedItemsOptic
+            {
+                slot = "CowsSlot";
+                item = "optic_Hamr";
+            };
+            class LinkedItemsAcc
+            {
+                slot = "PointerSlot";
+                item = "acc_pointer_IR";
+            };
+        };
+    };
+    class CUP_hgun_Duty;
+    class acr_a3_P07: CUP_hgun_Duty
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58P;
+    class acr_a3_Sa58P: CUP_arifle_Sa58P
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58P_des;
+    class acr_a3_Sa58P_camo: CUP_arifle_Sa58P_des
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58V;
+    class acr_a3_Sa58V: CUP_arifle_Sa58V
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58V_camo;
+    class acr_a3_Sa58V_camo: CUP_arifle_Sa58V_camo
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58RIS1;
+    class acr_a3_Sa58RIS1: CUP_arifle_Sa58RIS1
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58RIS1_des;
+    class acr_a3_Sa58RIS1_camo: CUP_arifle_Sa58RIS1_des
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58RIS2;
+    class acr_a3_Sa58RIS2: CUP_arifle_Sa58RIS2
+    {
+        scope = 1;
+    };
+    class CUP_arifle_Sa58RIS2_camo;
+    class acr_a3_Sa58RIS2_camo: CUP_arifle_Sa58RIS2_camo
+    {
+        scope = 1;
+    };
+
+    class CUP_arifle_Sa58RIS1_Aco_Laser;
+    class acr_a3_Sa58RIS1_Aco_Laser: CUP_arifle_Sa58RIS1_Aco_Laser {};
+    class CUP_arifle_Sa58RIS2_Arco_Laser;
+    class acr_a3_Sa58RIS2_Arco_Laser: CUP_arifle_Sa58RIS2_Arco_Laser {};
+    class CUP_arifle_Sa58RIS1_camo_Aco_Laser;
+    class acr_a3_Sa58RIS1_camo_Aco_Laser: CUP_arifle_Sa58RIS1_camo_Aco_Laser {};
+    class CUP_arifle_Sa58RIS2_camo_Arco_Laser;
+    class acr_a3_Sa58RIS2_camo_Arco_Laser: CUP_arifle_Sa58RIS2_camo_Arco_Laser {};
+    class CUP_hgun_Phantom;
+    class acr_a3_SP01: CUP_hgun_Phantom
+    {
+        scope = 1;
+    };
+    class Rifle_Long_Base_F;
+    class CUP_lmg_M60A4: Rifle_Long_Base_F {
+        magazines[] = {"CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M","CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M","acr_a3_100Rnd_762x51_B_M60E4"};
+    };
+    class acr_a3_M60E4: CUP_lmg_M60A4
+    {
+        scope = 1;
+    };
+};
+class cfgMagazines {
+    class CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M;
+    class acr_a3_100Rnd_762x51_B_M60E4: CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M {
+        scope = 1;
+    };
 };
 class cfgVehicles {
     class B_supplyCrate_F;
