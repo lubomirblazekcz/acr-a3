0818826035bf4895d1e86c7b5e5337393c0390a9
EvroMalarkey <evromalarkey@gmail.com>
2015-01-06 10:09:42 +0100

    Added: CUP Backpacks

--- Source/acr_a3/acr_a3_cfg/config.cpp
+++ Source/acr_a3/acr_a3_cfg/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {"ACR_A3_GearBox","ACR_A3_Backpack_Carryall_vz95","ACR_A3_Backpack_Kitbag_vz95","ACRP_Vojak01","ACRP_Vojak02","ACRP_Vojak07","ACRP_Vojak03","ACRP_Vojak04","ACRP_Vojak05","ACRP_Vojak10","ACRP_Vojak06","ACRP_Des_Vojak06","ACRP_Des_Vojak01","ACRP_Des_Vojak02","ACRP_Des_Vojak03","ACRP_Des_Vojak04","ACRP_Des_Vojak05","ACRP_Des_Vojak07","ACRP_Des_Vojak10","ACR_Vojak01","ACR_Vojak02","ACR_Vojak07","ACR_Vojak03","ACR_Vojak04","ACR_Vojak05","ACR_Sniper","ACR_Sniper2","ACR_Sniper_Des","ACR_Sniper2_Des","ACR_Vojak10","ACR_Vojak06","ACR_Vojak12","ACR_MOUT_Vojak01","ACR_MOUT_Vojak02","ACR_MOUT_Vojak07","ACR_MOUT_Vojak03","ACR_MOUT_Vojak04","ACR_MOUT_Vojak05","ACR_MOUT_Vojak10","ACR_MOUT_Vojak06","ACR_MOUT_Des_Vojak01","ACR_MOUT_Des_Vojak02","ACR_MOUT_Des_Vojak07","ACR_MOUT_Des_Vojak03","ACR_MOUT_Des_Vojak04","ACR_MOUT_Des_Vojak05","ACR_MOUT_Des_Vojak10","ACR_MOUT_Des_Vojak06","ACR_Des_Vojak01","ACR_Des_Vojak02","ACR_Des_Vojak07","ACR_Des_Vojak03","ACR_Des_Vojak04","ACR_Des_Vojak05","ACR_Des_Vojak10","ACR_Des_Vojak06","ACR_Pilot","ACR_Dustojnik","ACR_Neozbrojeny","ACR_Pilot_des","ACR_Dustojnik_des","ACR_Neozbrojeny_des"};
 		weapons[] = {"V_ACR_A3_PlateCarrierIA2_vz95","V_ACR_A3_PlateCarrier1_vz95","V_ACR_A3_PlateCarrier2_vz95","V_ACR_A3_PlateCarrier1_rgr","V_ACR_A3_PlateCarrier2_rgr","V_ACR_A3_PlateCarrier1_khk","V_ACR_A3_PlateCarrier2_khk","H_ACR_A3_HelmetIA_vz95","H_ACR_A3_HelmetIA_vz95_des","H_ACR_A3_HelmetB_rgr","H_ACR_A3_Beret_red","H_ACR_A3_Booniehat_vz95","H_ACR_A3_Cap_rgr","U_ACR_A3_CombatUniform","U_ACR_A3_CombatUniform_tshirt","U_ACR_A3_CombatUniform_vest","U_ACR_A3_CombatUniform_MOUT","U_ACR_A3_CombatUniform_MOUT_vest","U_ACR_A3_CombatUniform_Des","U_ACR_A3_CombatUniform_Des_tshirt","U_ACR_A3_CombatUniform_Des_vest","U_ACR_A3_HeliPilotCoveralls_rgr","U_ACR_A3_OfficerUniform_vz95","U_ACR_A3_SweaterUniform_vz95","U_ACR_A3_HeliPilotCoveralls_khk","U_ACR_A3_OfficerUniform_des","U_ACR_A3_SweaterUniform_des","U_ACR_A3_SniperGhillie","U_ACR_A3_SniperGhillie_Des","U_ACR_A3_CombatUniform_MOUT_Des","U_ACR_A3_CombatUniform_MOUT_Des_vest"};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"A3_Weapons_F","A3_Characters_F_BLUFOR", "acr_a3_dubbingradio"};
+		requiredAddons[] = {"A3_Weapons_F","A3_Characters_F_BLUFOR","CUP_Weapons_Backpacks","acr_a3_dubbingradio"};
 	};
 };
 class GenericNames
@@ -874,6 +874,29 @@ class cfgVehicles
 	class B_Kitbag_rgr_Exp;
 	class B_Kitbag_rgr;
 	// Batohy
+	class Bag_Base;
+    class CUP_B_ACRPara_Base: Bag_Base
+    {
+		hiddenSelections[] = {"Camo"};
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\backpack_acr_co.paa"};
+        hiddenSelectionsMaterials[] = {"\acr_a3\acr_a3_characters\data\ACR_Uniforma.rvmat"};
+    };
+    class CUP_B_ACRPara_m95: CUP_B_ACRPara_Base
+    {
+        displayName = "$STR_ACR_BACKPACK_A2";
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\backpack_acr_co.paa"};
+    };
+    class CUP_B_ACRScout_Base: Bag_Base
+    {
+		hiddenSelections[] = {"Camo"};
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\acr_equip2_co.paa"};
+        hiddenSelectionsMaterials[] = {"\acr_a3\acr_a3_characters\data\ACR_Uniforma.rvmat"};
+    };
+    class CUP_B_ACRScout_m95: CUP_B_ACRScout_Base
+    {
+        displayName = "$STR_ACR_KITBAG_A2";
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\acr_equip2_co.paa"};
+    };
 	class ACR_A3_Backpack_Carryall_vz95: B_Carryall_mcamo
 	{
 		scope = 2;
