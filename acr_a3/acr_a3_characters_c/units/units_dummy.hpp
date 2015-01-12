94cfcc0d062d991fd867262d2c2da00324888d32
EvroMalarkey <evromalarkey@gmail.com>
2015-01-12 18:44:13 +0100

    fix

--- acr_a3/acr_a3_characters_c/units/units_dummy.hpp
+++ acr_a3/acr_a3_characters_c/units/units_dummy.hpp
@@ -1,197 +1,197 @@
-class ACR_Vojak_01: B_ACR_A3_Soldier_01 {
+class ACR_Vojak01: B_ACR_A3_Soldier_01 {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_02: B_ACR_A3_Soldier_GL {
+class ACR_Vojak02: B_ACR_A3_Soldier_GL {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_03: B_ACR_A3_Soldier_AR {
+class ACR_Vojak03: B_ACR_A3_Soldier_AR {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_04: B_ACR_A3_Soldier_M {
+class ACR_Vojak04: B_ACR_A3_Soldier_M {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_05: B_ACR_A3_Soldier_medic {
+class ACR_Vojak05: B_ACR_A3_Soldier_medic {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_06: B_ACR_A3_Soldier_TL {
+class ACR_Vojak06: B_ACR_A3_Soldier_TL {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_07: B_ACR_A3_Soldier_repair {
+class ACR_Vojak07: B_ACR_A3_Soldier_repair {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_Vojak_10: B_ACR_A3_Soldier_MAT {
-    scope= 1;
-    scopeCurator =1;
-};
-
-class ACR_Des_Vojak_01: B_ACR_A3_Soldier_01_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_02: B_ACR_A3_Soldier_GL_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_03: B_ACR_A3_Soldier_AR_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_04: B_ACR_A3_Soldier_M_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_05: B_ACR_A3_Soldier_medic_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_06: B_ACR_A3_Soldier_TL_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_07: B_ACR_A3_Soldier_repair_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_Des_Vojak_10: B_ACR_A3_Soldier_MAT_des {
+class ACR_Vojak10: B_ACR_A3_Soldier_MAT {
     scope= 1;
     scopeCurator =1;
 };
 
-class ACR_MOUT_Vojak_01: B_ACR_A3_Soldier_01_mout {
+class ACR_Des_Vojak01: B_ACR_A3_Soldier_01_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_02: B_ACR_A3_Soldier_GL_mout {
+class ACR_Des_Vojak02: B_ACR_A3_Soldier_GL_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_03: B_ACR_A3_Soldier_AR_mout {
+class ACR_Des_Vojak03: B_ACR_A3_Soldier_AR_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_04: B_ACR_A3_Soldier_M_mout {
+class ACR_Des_Vojak04: B_ACR_A3_Soldier_M_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_05: B_ACR_A3_Soldier_medic_mout {
+class ACR_Des_Vojak05: B_ACR_A3_Soldier_medic_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_06: B_ACR_A3_Soldier_TL_mout {
+class ACR_Des_Vojak06: B_ACR_A3_Soldier_TL_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_07: B_ACR_A3_Soldier_repair_mout {
+class ACR_Des_Vojak07: B_ACR_A3_Soldier_repair_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACR_MOUT_Vojak_10: B_ACR_A3_Soldier_MAT_mout {
-    scope= 1;
-    scopeCurator =1;
-};
-
-class ACR_MOUT_Des_Vojak_01: B_ACR_A3_Soldier_01_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_02: B_ACR_A3_Soldier_GL_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_03: B_ACR_A3_Soldier_AR_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_04: B_ACR_A3_Soldier_M_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_05: B_ACR_A3_Soldier_medic_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_06: B_ACR_A3_Soldier_TL_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_07: B_ACR_A3_Soldier_repair_mout_des {
-    scope= 1;
-    scopeCurator =1;
-};
-class ACR_MOUT_Des_Vojak_10: B_ACR_A3_Soldier_MAT_mout_des {
+class ACR_Des_Vojak10: B_ACR_A3_Soldier_MAT_des {
     scope= 1;
     scopeCurator =1;
 };
 
-class ACRP_Vojak_01: B_ACR_A3_Recon {
+class ACR_MOUT_Vojak01: B_ACR_A3_Soldier_01_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_02: B_ACR_A3_Recon_JTAC {
+class ACR_MOUT_Vojak02: B_ACR_A3_Soldier_GL_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_03: B_ACR_A3_Recon_AR {
+class ACR_MOUT_Vojak03: B_ACR_A3_Soldier_AR_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_04: B_ACR_A3_Recon_M {
+class ACR_MOUT_Vojak04: B_ACR_A3_Soldier_M_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_05: B_ACR_A3_Recon_medic {
+class ACR_MOUT_Vojak05: B_ACR_A3_Soldier_medic_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_06: B_ACR_A3_Recon_TL {
+class ACR_MOUT_Vojak06: B_ACR_A3_Soldier_TL_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_07: B_ACR_A3_Recon_exp {
+class ACR_MOUT_Vojak07: B_ACR_A3_Soldier_repair_mout {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Vojak_10: B_ACR_A3_Recon_MAT {
+class ACR_MOUT_Vojak10: B_ACR_A3_Soldier_MAT_mout {
     scope= 1;
     scopeCurator =1;
 };
 
-class ACRP_Des_Vojak_01: B_ACR_A3_Recon_des {
+class ACR_MOUT_Des_Vojak01: B_ACR_A3_Soldier_01_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_02: B_ACR_A3_Recon_JTAC_des {
+class ACR_MOUT_Des_Vojak02: B_ACR_A3_Soldier_GL_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_03: B_ACR_A3_Recon_AR_des {
+class ACR_MOUT_Des_Vojak03: B_ACR_A3_Soldier_AR_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_04: B_ACR_A3_Recon_M_des {
+class ACR_MOUT_Des_Vojak04: B_ACR_A3_Soldier_M_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_05: B_ACR_A3_Recon_medic_des {
+class ACR_MOUT_Des_Vojak05: B_ACR_A3_Soldier_medic_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_06: B_ACR_A3_Recon_TL_des {
+class ACR_MOUT_Des_Vojak06: B_ACR_A3_Soldier_TL_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_07: B_ACR_A3_Recon_exp_des {
+class ACR_MOUT_Des_Vojak07: B_ACR_A3_Soldier_repair_mout_des {
     scope= 1;
     scopeCurator =1;
 };
-class ACRP_Des_Vojak_10: B_ACR_A3_Recon_MAT_des {
+class ACR_MOUT_Des_Vojak10: B_ACR_A3_Soldier_MAT_mout_des {
+    scope= 1;
+    scopeCurator =1;
+};
+
+class ACRP_Vojak01: B_ACR_A3_Recon {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak02: B_ACR_A3_Recon_JTAC {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak03: B_ACR_A3_Recon_AR {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak04: B_ACR_A3_Recon_M {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak05: B_ACR_A3_Recon_medic {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak06: B_ACR_A3_Recon_TL {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak07: B_ACR_A3_Recon_exp {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Vojak10: B_ACR_A3_Recon_MAT {
+    scope= 1;
+    scopeCurator =1;
+};
+
+class ACRP_Des_Vojak01: B_ACR_A3_Recon_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak02: B_ACR_A3_Recon_JTAC_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak03: B_ACR_A3_Recon_AR_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak04: B_ACR_A3_Recon_M_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak05: B_ACR_A3_Recon_medic_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak06: B_ACR_A3_Recon_TL_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak07: B_ACR_A3_Recon_exp_des {
+    scope= 1;
+    scopeCurator =1;
+};
+class ACRP_Des_Vojak10: B_ACR_A3_Recon_MAT_des {
     scope= 1;
     scopeCurator =1;
 };
