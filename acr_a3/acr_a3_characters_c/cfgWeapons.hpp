cee5dc0ff5421d3bc77c94f19f534357a2034dfb
taurus <xxxTaurus@gmail.com>
2015-08-29 13:38:08 +0200

    helma bez brejlicek

--- acr_a3/acr_a3_characters_c/cfgWeapons.hpp
+++ acr_a3/acr_a3_characters_c/cfgWeapons.hpp
@@ -327,27 +327,6 @@ class cfgWeapons
             hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip2_des_co.paa"}; /// what texture is going to be used
         };
     };
-	class H_ACR_A3_HelmetIA_Goggles_wocover: ItemCore
-        {
-        scope = 2; /// scope needs to be 2 to have a visible class
-        displayName  = "STR_ACR_MICH2000WOCOVER_GOGGLES"; /// how would the stuff be displayed in inventory and on ground
-        picture = "\acr_a3\acr_a3_characters\data\UI\ikona_ACR_helma_WOCOVERGOGGLES"; /// this looks fairly similar
-        model   = "\acr_a3\acr_a3_characters\mich2000wocover_ACR.p3d"; /// what model does the cap use
-		author = "$STR_ACR";
-        hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
-        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
-        class ItemInfo : HeadgearItem
-        {
-            mass = 40; /// combined weight and volume of the cap, this equals to single magazine
-            uniformModel = "\acr_a3\acr_a3_characters\mich2000wocover_ACR.p3d";  /// what model is used for this cap
-            allowedSlots[] = {901,605};   /// this cap is so small and flexible that it fits everywhere
-            modelSides[] = {6}; /// available for all sides
-            armor = 4;
-            passThrough = 0.5;
-            hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
-            hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
-        };
-    };
 	class H_ACR_A3_HelmetIA_Goggles_vz95: ItemCore
         {
         scope = 2; /// scope needs to be 2 to have a visible class
@@ -392,23 +371,44 @@ class cfgWeapons
     };
     class H_ACR_A3_HelmetB_rgr: ItemCore
     {
-        scope = 2;
-        weaponPoolAvailable = 1;
-        displayName = "$STR_ACR_ECH_rgr";
-        picture = "\A3\characters_f\Data\UI\icon_H_HelmetB_CA.paa";
-        model = "\A3\Characters_F\BLUFOR\headgear_b_helmet_ballistic";
+        scope = 2; /// scope needs to be 2 to have a visible class
+        displayName  = "$STR_ACR_MICH2000WOCOVER"; /// how would the stuff be displayed in inventory and on ground
+        picture = "\acr_a3\acr_a3_characters\data\UI\ikona_ACR_helma_WOCOVERGOGGLES.paa"; /// this looks fairly similar
+        model   = "\acr_a3\acr_a3_characters\mich2000wocover_ACR.p3d"; /// what model does the cap use
 		author = "$STR_ACR";
-        hiddenSelections[] = {"camo"};
-        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\ACR_Prilba2.paa"};
-        class ItemInfo: HeadGearItem
+        hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
+        class ItemInfo : HeadgearItem
         {
-			mass = 50;
-			uniformModel = "\A3\Characters_F\BLUFOR\headgear_b_helmet_ballistic";
-			modelSides[] = {6};
+            mass = 40; /// combined weight and volume of the cap, this equals to single magazine
+            uniformModel = "\acr_a3\acr_a3_characters\mich2000wocover_ACR.p3d";  /// what model is used for this cap
             allowedSlots[] = {901,605};   /// this cap is so small and flexible that it fits everywhere
-			armor = 5;
-			passThrough = 0.5;
-            hiddenSelections[] = {"camo"};
+            modelSides[] = {6}; /// available for all sides
+            armor = 4;
+            passThrough = 0.5;
+            hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+            hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
+        };
+    };
+	class H_ACR_A3_HelmetB_rgl_Goggles: ItemCore
+        {
+        scope = 2; /// scope needs to be 2 to have a visible class
+        displayName  = "$STR_ACR_MICH2000WOCOVER_GOGGLES"; /// how would the stuff be displayed in inventory and on ground
+        picture = "\acr_a3\acr_a3_characters\data\UI\ikona_ACR_helma_WOCOVERGOGGLES.paa"; /// this looks fairly similar
+        model   = "\acr_a3\acr_a3_characters\mich2000wocovernogoggles_ACR.p3d"; /// what model does the cap use
+		author = "$STR_ACR";
+        hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+        hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
+        class ItemInfo : HeadgearItem
+        {
+            mass = 40; /// combined weight and volume of the cap, this equals to single magazine
+            uniformModel = "\acr_a3\acr_a3_characters\mich2000wocovernogoggles_ACR.p3d";  /// what model is used for this cap
+            allowedSlots[] = {901,605};   /// this cap is so small and flexible that it fits everywhere
+            modelSides[] = {6}; /// available for all sides
+            armor = 4;
+            passThrough = 0.5;
+            hiddenSelections[] = {"camo"}; /// what selection in model could have different textures
+            hiddenSelectionsTextures[] = {"\acr_a3\acr_a3_characters\data\equip3_co.paa"}; /// what texture is going to be used
         };
     };
     class H_ACR_A3_Beret_red: ItemCore
