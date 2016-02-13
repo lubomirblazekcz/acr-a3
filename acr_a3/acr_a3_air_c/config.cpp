064607f3b6a2d18ca071a23f88d44b6e67d26a93
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2016-02-13 16:22:44 +0100

    moving to gitlab

--- acr_a3/acr_a3_air_c/config.cpp
+++
@@ -1,9 +0,0 @@
-class CfgPatches {
-
-	class acr_a3_air_c {
-		units[] = { };
-		weapons[] = { };
-		requiredVersion = 0.100000;
-		requiredAddons[] = { };
-	};
-};
--- acr_a3/acr_a3_tfar/config.cpp
+++
@@ -1,8 +0,0 @@
-class CfgPatches {
-	class acr_a3_tfar {
-        units[] = { };
-        weapons[] = { };
-        requiredVersion = 0.100000;
-		requiredAddons[] = {"acr_a3_tfar_c"};
-	};
-};
